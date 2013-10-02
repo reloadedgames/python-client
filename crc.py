50572448108ed535eac9c8a59ac0bcb5a742c62c
Lance McNearney <lance@mcnearney.net>
2013-10-02 16:36:42 -0700

    Return a dictionary

--- crc.py
+++ crc.py
@@ -1,27 +1,42 @@
 import binascii
 import os
 
-path = 'C:\Users\lmcnearney\Downloads\Package\\'
-chunk_size = 1048576
 
-if not os.path.isdir(path):
-    exit('You must specify a directory')
+def calculate_chunks(path, chunk_size):
+    if not os.path.isdir(path):
+        exit('You must specify a directory')
 
-for root, dirs, files in os.walk(path):
-    for name in files:
-        file_path = os.path.join(root, name)
-        print file_path.replace(path, '')
+    chunks = {}
 
-        with open(file_path, 'r+b') as f:
-            part = 0
-            chunk = f.read(chunk_size)
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            file_path = os.path.join(root, name)
+            relative_path = file_path.replace(path, '')
+            chunks[relative_path] = {
+                'pieces': [],
+                'size': os.path.getsize(file_path)
+            }
 
-            while chunk:
-                part += 1
-                length = chunk.__len__()
-                crc = binascii.crc32(chunk) & 0xffffffff
-                print 'Part = {0}, Length = {1}, CRC = {2}, Hex = {2:x}'.format(part, length, crc)
-
+            with open(file_path, 'r+b') as f:
                 chunk = f.read(chunk_size)
+
+                while chunk:
+                    crc = binascii.crc32(chunk) & 0xffffffff
+                    chunks[relative_path]['pieces'].append(crc)
+                    chunk = f.read(chunk_size)
+
+    return chunks
+
+if __name__ == '__main__':
+    _path = 'C:\Users\lmcnearney\Downloads\Package\\'
+    _chunk_size = 1048576
+
+    result = calculate_chunks(_path, _chunk_size)
+
+    for key in result:
+        print '{0} - {1} bytes'.format(key, result[key]['size'])
+
+        for crc in result[key]['pieces']:
+            print '=> {0} = {0:x}'.format(crc)
 
         print ''
